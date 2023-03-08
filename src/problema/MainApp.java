a6ae936603f137ceb7e053bf1444e04bf0ebdc58
SergiuAnd09 <sergiu.oprea@student.upt.ro>
2023-03-08 15:09:46 +0200

    first

---
+++ src/problema/MainApp.java
@@ -0,0 +1,15 @@
+package problema;
+import java.util.Scanner;
+
+public class MainApp {
+	
+	public static void main(String []args) {
+		
+		int x;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Give a suitable number: ");
+		x = scanner.nextInt();
+		System.out.println("Well done.");
+	}
+
+}
